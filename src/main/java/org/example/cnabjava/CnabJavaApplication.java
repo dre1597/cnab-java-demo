df49c4ad1dfcc7e49adde718f317fba414b8282f
André Pereira <andrepereira97@yahoo.com>
2025-05-11 19:15:18 -0300

    :tada: init project

---
+++ src/main/java/org/example/cnabjava/CnabJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.cnabjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CnabJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/cnabjava/CnabJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cnabjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
