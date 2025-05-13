b7ed4a0b1baa00d91d3bbbc51d7d330ed1290d28
André Pereira <andrepereira97@yahoo.com>
2025-05-12 23:59:28 -0300

    :sparkles: add new fields

--- src/main/java/org/example/cnabjava/entities/Cnab240Header.java
+++ src/main/java/org/example/cnabjava/entities/Cnab240Header.java
@@ -15,6 +15,9 @@ public class Cnab240Header {
   private UUID id = UUID.randomUUID();
 
   @Column(nullable = false)
+  private String cnpj;
+
+  @Column(nullable = false)
   private String bankCode;
 
   @Column(nullable = false)
@@ -33,7 +36,22 @@ public class Cnab240Header {
   private String digit;
 
   @Column(nullable = false)
+  private String companyName;
+
+  @Column(nullable = false)
   private String date;
+
+  @Column(nullable = false)
+  private String time;
+
+  @Column(nullable = false)
+  private String sequentialNumber;
+
+  @Column(nullable = false)
+  private String version;
+
+  @Column(nullable = false)
+  private int numberOfRegisters;
 
   @CreationTimestamp
   private Instant createdAt;
@@ -46,24 +64,36 @@ public class Cnab240Header {
 
   public Cnab240Header(
       final UUID id,
+      final String cnpj,
       final String bankCode,
       final String agencyCode,
       final String agencyDigit,
       final String accountNumber,
       final String accountDigit,
       final String digit,
+      final String companyName,
       final String date,
+      final String time,
+      final String sequentialNumber,
+      final String version,
+      final int numberOfRegisters,
       final Instant createdAt,
       final Instant updatedAt
   ) {
     this.id = id;
+    this.cnpj = cnpj;
     this.bankCode = bankCode;
     this.agencyCode = agencyCode;
     this.agencyDigit = agencyDigit;
     this.accountNumber = accountNumber;
     this.accountDigit = accountDigit;
     this.digit = digit;
+    this.companyName = companyName;
     this.date = date;
+    this.time = time;
+    this.sequentialNumber = sequentialNumber;
+    this.version = version;
+    this.numberOfRegisters = numberOfRegisters;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
@@ -74,6 +104,14 @@ public class Cnab240Header {
 
   public void setId(final UUID id) {
     this.id = id;
+  }
+
+  public String getCnpj() {
+    return this.cnpj;
+  }
+
+  public void setCnpj(final String cnpj) {
+    this.cnpj = cnpj;
   }
 
   public String getBankCode() {
@@ -124,12 +162,52 @@ public class Cnab240Header {
     this.digit = digit;
   }
 
+  public String getCompanyName() {
+    return this.companyName;
+  }
+
+  public void setCompanyName(final String companyName) {
+    this.companyName = companyName;
+  }
+
   public String getDate() {
     return this.date;
   }
 
   public void setDate(final String date) {
     this.date = date;
+  }
+
+  public String getTime() {
+    return this.time;
+  }
+
+  public void setTime(final String time) {
+    this.time = time;
+  }
+
+  public String getSequentialNumber() {
+    return this.sequentialNumber;
+  }
+
+  public void setSequentialNumber(final String sequentialNumber) {
+    this.sequentialNumber = sequentialNumber;
+  }
+
+  public String getVersion() {
+    return this.version;
+  }
+
+  public void setVersion(final String version) {
+    this.version = version;
+  }
+
+  public int getNumberOfRegisters() {
+    return this.numberOfRegisters;
+  }
+
+  public void setNumberOfRegisters(final int numberOfRegisters) {
+    this.numberOfRegisters = numberOfRegisters;
   }
 
   public Instant getCreatedAt() {
@@ -152,13 +230,19 @@ public class Cnab240Header {
   public String toString() {
     return "CnabHeader{" +
         "id=" + id +
+        ", cnpj='" + cnpj + '\'' +
         ", bankCode='" + bankCode + '\'' +
         ", agencyCode='" + agencyCode + '\'' +
         ", agencyDigit='" + agencyDigit + '\'' +
         ", accountNumber='" + accountNumber + '\'' +
         ", accountDigit='" + accountDigit + '\'' +
         ", digit='" + digit + '\'' +
+        ", companyName='" + companyName + '\'' +
         ", date='" + date + '\'' +
+        ", time='" + time + '\'' +
+        ", sequentialNumber='" + sequentialNumber + '\'' +
+        ", version='" + version + '\'' +
+        ", numberOfRegisters=" + numberOfRegisters +
         ", createdAt=" + createdAt +
         ", updatedAt=" + updatedAt +
         '}';
--- src/test/java/org/example/cnabjava/entities/Cnab240HeaderTest.java
+++ src/test/java/org/example/cnabjava/entities/Cnab240HeaderTest.java
@@ -11,36 +11,54 @@ class Cnab240HeaderTest {
   @Test
   void shouldCreateUsingAllArgsConstructor() {
     final var id = UUID.randomUUID();
+    final var cnpj = "any_cnpj";
     final var bankCode = "any_bank_code";
     final var agencyCode = "any_agency_code";
     final var accountNumber = "any_account_number";
     final var accountDigit = "any_account_digit";
     final var agencyDigit = "any_agency_digit";
     final var digit = "any_digit";
+    final var companyName = "any_company_name";
     final var date = "any_date";
+    final var time = "any_time";
+    final var sequentialNumber = "any_sequential_number";
+    final var version = "any_version";
+    final var numberOfRegisters = 1;
     final var now = Instant.now();
 
     final var header = new Cnab240Header(
         id,
+        cnpj,
         bankCode,
         agencyCode,
         agencyDigit,
         accountNumber,
         accountDigit,
         digit,
+        companyName,
         date,
+        time,
+        sequentialNumber,
+        version,
+        numberOfRegisters,
         now,
         now
     );
 
     assertThat(header.getId()).isEqualTo(id);
+    assertThat(header.getCnpj()).isEqualTo(cnpj);
     assertThat(header.getBankCode()).isEqualTo(bankCode);
     assertThat(header.getAgencyCode()).isEqualTo(agencyCode);
     assertThat(header.getAgencyDigit()).isEqualTo(agencyDigit);
     assertThat(header.getAccountNumber()).isEqualTo(accountNumber);
     assertThat(header.getAccountDigit()).isEqualTo(accountDigit);
-    assertThat(header.getDate()).isEqualTo(date);
     assertThat(header.getDigit()).isEqualTo(digit);
+    assertThat(header.getCompanyName()).isEqualTo(companyName);
+    assertThat(header.getDate()).isEqualTo(date);
+    assertThat(header.getTime()).isEqualTo(time);
+    assertThat(header.getSequentialNumber()).isEqualTo(sequentialNumber);
+    assertThat(header.getVersion()).isEqualTo(version);
+    assertThat(header.getNumberOfRegisters()).isEqualTo(numberOfRegisters);
     assertThat(header.getCreatedAt()).isEqualTo(now);
     assertThat(header.getUpdatedAt()).isEqualTo(now);
   }
@@ -48,35 +66,53 @@ class Cnab240HeaderTest {
   @Test
   void shouldSetAndGetAllFields() {
     final var id = UUID.randomUUID();
+    final var cnpj = "other_cnpj";
     final var bankCode = "other_bank_code";
     final var agencyCode = "other_agency_code";
     final var accountNumber = "other_account_number";
     final var accountDigit = "other_account_digit";
     final var agencyDigit = "other_agency_digit";
     final var digit = "other_digit";
+    final var companyName = "other_company_name";
     final var date = "other_date";
+    final var time = "other_time";
+    final var sequentialNumber = "other_sequential_number";
+    final var version = "other_version";
+    final var numberOfRegisters = 2;
     final var now = Instant.now();
 
     final var header = new Cnab240Header();
     header.setId(id);
+    header.setCnpj(cnpj);
     header.setBankCode(bankCode);
     header.setAgencyCode(agencyCode);
     header.setAccountNumber(accountNumber);
     header.setAccountDigit(accountDigit);
     header.setAgencyDigit(agencyDigit);
-    header.setDate(date);
     header.setDigit(digit);
+    header.setCompanyName(companyName);
+    header.setDate(date);
+    header.setTime(time);
+    header.setSequentialNumber(sequentialNumber);
+    header.setVersion(version);
+    header.setNumberOfRegisters(numberOfRegisters);
     header.setCreatedAt(now);
     header.setUpdatedAt(now);
 
     assertThat(header.getId()).isEqualTo(id);
+    assertThat(header.getCnpj()).isEqualTo(cnpj);
     assertThat(header.getBankCode()).isEqualTo(bankCode);
     assertThat(header.getAgencyCode()).isEqualTo(agencyCode);
     assertThat(header.getAgencyDigit()).isEqualTo(agencyDigit);
     assertThat(header.getAccountNumber()).isEqualTo(accountNumber);
     assertThat(header.getAccountDigit()).isEqualTo(accountDigit);
-    assertThat(header.getDate()).isEqualTo(date);
     assertThat(header.getDigit()).isEqualTo(digit);
+    assertThat(header.getCompanyName()).isEqualTo(companyName);
+    assertThat(header.getDate()).isEqualTo(date);
+    assertThat(header.getTime()).isEqualTo(time);
+    assertThat(header.getSequentialNumber()).isEqualTo(sequentialNumber);
+    assertThat(header.getVersion()).isEqualTo(version);
+    assertThat(header.getNumberOfRegisters()).isEqualTo(numberOfRegisters);
     assertThat(header.getCreatedAt()).isEqualTo(now);
     assertThat(header.getUpdatedAt()).isEqualTo(now);
   }
@@ -84,37 +120,55 @@ class Cnab240HeaderTest {
   @Test
   void shouldGenerateToString() {
     final var id = UUID.randomUUID();
+    final var cnpj = "other_cnpj";
     final var bankCode = "other_bank_code";
     final var agencyCode = "other_agency_code";
     final var accountNumber = "other_account_number";
     final var accountDigit = "other_account_digit";
     final var agencyDigit = "other_agency_digit";
     final var digit = "other_digit";
+    final var companyName = "other_company_name";
     final var date = "other_date";
+    final var time = "other_time";
+    final var sequentialNumber = "other_sequential_number";
+    final var version = "other_version";
+    final var numberOfRegisters = 2;
     final var now = Instant.now();
 
     final var header = new Cnab240Header(
         id,
+        cnpj,
         bankCode,
         agencyCode,
         agencyDigit,
         accountNumber,
         accountDigit,
         digit,
+        companyName,
         date,
+        time,
+        sequentialNumber,
+        version,
+        numberOfRegisters,
         now,
         now
     );
 
     assertThat(header.toString()).hasToString("CnabHeader{" +
         "id=" + id +
+        ", cnpj='" + cnpj + '\'' +
         ", bankCode='" + bankCode + '\'' +
         ", agencyCode='" + agencyCode + '\'' +
         ", agencyDigit='" + agencyDigit + '\'' +
         ", accountNumber='" + accountNumber + '\'' +
         ", accountDigit='" + accountDigit + '\'' +
         ", digit='" + digit + '\'' +
+        ", companyName='" + companyName + '\'' +
         ", date='" + date + '\'' +
+        ", time='" + time + '\'' +
+        ", sequentialNumber='" + sequentialNumber + '\'' +
+        ", version='" + version + '\'' +
+        ", numberOfRegisters=" + numberOfRegisters +
         ", createdAt=" + now +
         ", updatedAt=" + now +
         '}');
